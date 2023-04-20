7f25e8943e0aaa3dfb93f8509045576e3d46d329
PedroTejon <pedrohtejon@gmail.com>
2023-04-20 16:19:58 -0300

    fix: Adicionei Soilwater_L2-4 às propriedades utilizadas nas predições

--- oleo.py
+++ oleo.py
@@ -39,15 +39,16 @@ def analise_propria(dados_treino, colunas):
 
 
 def previsao_kaggle(dados_treino, dados_teste, colunas):
-    # Geração de submission
+    # Treinamento do modelo
     modelo = RandomForestRegressor(n_estimators=100, max_depth=5, random_state=1)
     modelo.fit(dados_treino[colunas],  dados_treino['production'])
     dados_teste['production'] = modelo.predict(dados_teste[colunas])
 
+    # Criação do Gráfico para análise
     dados_teste['date'] = to_date(dados_teste)
     dados_treino['date'] = to_date(dados_treino)
     dados_todos = concat([dados_treino, dados_teste])
-    figura, eixos = subplots(figsize=(12, 10))
+    _, eixos = subplots(figsize=(12, 10))
     lineplot(x='date', y='production', data=dados_todos, ax=eixos).figure.savefig('graphs/production_by_year.png')
 
     return dados_teste
@@ -57,8 +58,9 @@ def main():
     dados_treino = read_csv('datasets/train new.csv')
     dados_teste = read_csv('datasets/test new.csv')
 
-    previsoes = previsao_kaggle(dados_treino, dados_teste, ['field','age','type','year','month','temperature','dewpoint','windspeed','Soilwater_L1','Precipitation','temperature_b1','temperature_b2','temperature_b3','temperature_b4','temperature_b5','temperature_b6','temperature_b7','temperature_b8','temperature_b9','temperature_b10','temperature_b11','temperature_b12','dewpoint_b1','dewpoint_b2','dewpoint_b3','dewpoint_b4','dewpoint_b5','dewpoint_b6','dewpoint_b7','dewpoint_b8','dewpoint_b9','dewpoint_b10','dewpoint_b11','dewpoint_b12','windspeed_b1','windspeed_b2','windspeed_b3','windspeed_b4','windspeed_b5','windspeed_b6','windspeed_b7','windspeed_b8','windspeed_b9','windspeed_b10','windspeed_b11','windspeed_b12','Soilwater_L1_b1','Soilwater_L1_b2','Soilwater_L1_b3','Soilwater_L1_b4','Soilwater_L1_b5','Soilwater_L1_b6','Soilwater_L1_b7','Soilwater_L1_b8','Soilwater_L1_b9','Soilwater_L1_b10','Soilwater_L1_b11','Soilwater_L1_b12','Precipitation_b1','Precipitation_b2','Precipitation_b3','Precipitation_b4','Precipitation_b5','Precipitation_b6','Precipitation_b7','Precipitation_b8','Precipitation_b9','Precipitation_b10','Precipitation_b11','Precipitation_b12'])
-    analise_propria(dados_treino, ['field','age','type','year','month','temperature','dewpoint','windspeed','Soilwater_L1','Precipitation','temperature_b1','temperature_b2','temperature_b3','temperature_b4','temperature_b5','temperature_b6','temperature_b7','temperature_b8','temperature_b9','temperature_b10','temperature_b11','temperature_b12','dewpoint_b1','dewpoint_b2','dewpoint_b3','dewpoint_b4','dewpoint_b5','dewpoint_b6','dewpoint_b7','dewpoint_b8','dewpoint_b9','dewpoint_b10','dewpoint_b11','dewpoint_b12','windspeed_b1','windspeed_b2','windspeed_b3','windspeed_b4','windspeed_b5','windspeed_b6','windspeed_b7','windspeed_b8','windspeed_b9','windspeed_b10','windspeed_b11','windspeed_b12','Soilwater_L1_b1','Soilwater_L1_b2','Soilwater_L1_b3','Soilwater_L1_b4','Soilwater_L1_b5','Soilwater_L1_b6','Soilwater_L1_b7','Soilwater_L1_b8','Soilwater_L1_b9','Soilwater_L1_b10','Soilwater_L1_b11','Soilwater_L1_b12','Precipitation_b1','Precipitation_b2','Precipitation_b3','Precipitation_b4','Precipitation_b5','Precipitation_b6','Precipitation_b7','Precipitation_b8','Precipitation_b9','Precipitation_b10','Precipitation_b11','Precipitation_b12'])
+    props = ['field','age','type','year','month','temperature','dewpoint','windspeed','Soilwater_L1', 'Soilwater_L2', 'Soilwater_L3', 'Soilwater_L4','Precipitation','temperature_b1','temperature_b2','temperature_b3','temperature_b4','temperature_b5','temperature_b6','temperature_b7','temperature_b8','temperature_b9','temperature_b10','temperature_b11','temperature_b12','dewpoint_b1','dewpoint_b2','dewpoint_b3','dewpoint_b4','dewpoint_b5','dewpoint_b6','dewpoint_b7','dewpoint_b8','dewpoint_b9','dewpoint_b10','dewpoint_b11','dewpoint_b12','windspeed_b1','windspeed_b2','windspeed_b3','windspeed_b4','windspeed_b5','windspeed_b6','windspeed_b7','windspeed_b8','windspeed_b9','windspeed_b10','windspeed_b11','windspeed_b12','Soilwater_L1_b1','Soilwater_L1_b2','Soilwater_L1_b3','Soilwater_L1_b4','Soilwater_L1_b5','Soilwater_L1_b6','Soilwater_L1_b7','Soilwater_L1_b8','Soilwater_L1_b9','Soilwater_L1_b10','Soilwater_L1_b11','Soilwater_L1_b12','Precipitation_b1','Precipitation_b2','Precipitation_b3','Precipitation_b4','Precipitation_b5','Precipitation_b6','Precipitation_b7','Precipitation_b8','Precipitation_b9','Precipitation_b10','Precipitation_b11','Precipitation_b12']
+    previsoes = previsao_kaggle(dados_treino, dados_teste, props)
+    analise_propria(dados_treino, props)
 
     res_submissao = DataFrame({'Id': previsoes.Id, 'production': previsoes.production})
     res_submissao.to_csv('submission.csv', index=False)
